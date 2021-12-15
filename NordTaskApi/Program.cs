f4769c346f0418603bab703b7fd13dad05236637
Dziarskiheenk <dziarskiheenk@gmail.com>
2021-12-15 14:24:02 +0100

    added JWT auth set up the same port for https (angular will be recognizing this pot and will send access token)

--- NordTaskApi/Program.cs
+++ NordTaskApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = "https://dev-94u897vb.us.auth0.com";
+    options.Audience = "nord-task-api";
+    options.TokenValidationParameters.NameClaimType = ClaimTypes.Email;
+});
 
 var app = builder.Build();
 
